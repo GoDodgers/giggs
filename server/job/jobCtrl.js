3a6c03bc072abb4a4f14de03a4399a14d25fb32c
Alberto M Esquivias <alberto.m.esquivias@gmail.com>
2016-09-20 11:23:43 -0700

    [feat] begins testing to add new job to db

--- server/job/jobCtrl.js
+++ server/job/jobCtrl.js
@@ -1,5 +1,23 @@
-var Job = require('./jobModel.js');
+const Job = require('./jobModel');
 
-module.exports.helloWorld = function(req, res) {
-	res.send('hello jobs');
+module.exports = {
+
+	helloWorld: (req, res) => {
+		res.send('hello Jobs');
+	},
+
+	createJob: (req, res) => {
+		/*var newJob = {
+			jobName: req.body.username,
+			openings: req.body.openings,
+			description: req.body.description,
+			max_price: req.body.max_price,
+			location_lat: req.body.location_lat,
+			location_lng: req.body.location_lng,
+			deadline: req.body.deadline,
+		}*/
+		Job.build(req.body)
+			.then(data => res.status(200).send(data))
+			.catch(error => error).save();
+	},
 };
--- server/job/jobModel.js
+++ server/job/jobModel.js
@@ -1,12 +1,10 @@
-var Sequelize = require('sequelize');
-var connection = require('../db/connection.js');
-var Categories = require('../category/categoryModel.js');
-console.log('requiring users');
-var Users = require('../user/userModel.js');
+const Sequelize = require('sequelize');
+const connection = require('../db/connection');
+const Categories = require('../category/categoryModel');
+const Users = require('../user/userModel');
 
-console.log('creating jobs');
-var Jobs = connection.define('Jobs', {
-	name: {
+const Jobs = connection.define('Jobs', {
+	jobName: {
 		type: Sequelize.STRING,
 	},
 
@@ -18,17 +16,17 @@ var Jobs = connection.define('Jobs', {
 	description: {
 		type: Sequelize.STRING,
 		len: [0, 255],
-		msg: "Description must be between 0 & 250 characters.",
+		msg: 'Description must be between 0 & 250 characters.',
 
 	},
 
 	category_id: {
 		type: Sequelize.INTEGER,
 		references: {
-			 // This is the reference to another model
-			 model: Categories,
-			 // This is the column name of the referenced model
-			 key: 'id',
+			// This is the reference to another model
+			model: Categories,
+			// This is the column name of the referenced model
+			key: 'id',
 		},
 	},
 
